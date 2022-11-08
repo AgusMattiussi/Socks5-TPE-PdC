df937134ba0740a004e322f72fdad201e29e52ce
Manuel <mdizenhaus@itba.edu.ar>
2022-11-08 12:15:49 -0300

    connection parser midway

---
+++ src/parsers/conn_parser.c
@@ -0,0 +1,77 @@
+#include "conn_parser.h"
+#include "../buffer/buffer.h"
+#include "../selector/selector.h"
+#include "../conn_handler.h"
+
+#include <stdio.h>
+
+bool check_if_valid_method(uint8_t byte){
+    return byte == NO_AUTH || byte == USER_PASS;
+}
+
+void start_connection_parser(struct conn_parser * parser){
+    parser->to_parse = 0;
+    parser->state = CONN_VERSION; // Start of msg
+    parser->auth = NO_METHODS;
+    parser->buff = NULL;
+}
+
+void conn_parse_byte(struct conn_parser * parser, uint8_t to_parse){
+    switch(to_parse){
+        //What state are we in, and what does this byte mean in that context
+        case CONN_VERSION:
+            if(to_parse == SOCKS_VERSION){
+                parser->state = CONN_NMETHODS;
+                break;
+            }
+            else{
+                parser->state = CONN_ERROR;
+                fprintf(STDOUT_FILENO, "Invalid socks version.");
+                break;
+            }
+        case CONN_NMETHODS:
+            parser->state = to_parse == 0? CONN_DONE:CONN_METHODS;
+            break;
+        case CONN_METHODS:
+            if(to_parse != GSSAPI){
+                bool valid_method = check_if_valid_method(to_parse);
+                if(valid_method){
+                    parser->auth = (enum auth_method) to_parse;
+                }
+                else{
+                    fprintf(STDOUT_FILENO, "Invalid auth method provided.");
+                    parser->state = CONN_ERROR;
+                }
+            }
+            else{
+                fprintf(STDOUT_FILENO, "Current project version does not support auth via GSS_API yet. Sorry.");
+                parser->state = CONN_ERROR;
+            }
+            break;
+        case CONN_DONE: case CONN_ERROR: break;
+        default: fprintf(stdout, "Error at parsing. Please try again.");
+    }
+}
+
+enum conn_state conn_parse_full(struct conn_parser * parser, buffer * buff){
+    // Take byte to byte and use helper function to pass between states
+    // We can use buffer functions directly to know when to stop reading
+    while(buffer_can_read(buff)){
+        uint8_t to_parse = buffer_read(buff);
+        conn_parse_byte(parser, to_parse);
+        if(parser->state == CONN_ERROR){
+            fprintf(stdout, "Error while parsing input."); //Might be excesive
+            return CONN_ERROR;
+        }
+        else if(parser->state == CONN_DONE){
+            //For debugging purposes
+            fprintf(stdout, "Connection OK!");
+            return CONN_DONE;
+        }
+    }
+    if(parser->state != CONN_ERROR && parser->state != CONN_DONE){
+        //For debugging purposes
+        fprintf(stdout, "Shouldn't reach this state, but parsing went wrong with no aparent error?");
+    }
+    return parser->state;
+}
--- src/socks5/socks5.c
+++ src/socks5/socks5.c
@@ -1,10 +1,42 @@
 #include "socks5.h"
 #include "../stm/stm.h"
+#include "../selector/selector.h"
+
+void conn_read_init(struct selector_key * key){
+    struct socks_conn_model * connection = (socks_conn_model *)key->data;
+    // TODO: Where to parse input? 
+}
+
+static enum socks_state conn_read(struct selector_key * key){
+    struct socks_conn_model * connection = (socks_conn_model *)key->data;
+
+    // We need to receive bytes from socket (use recv for this)
+    // Reminder: buff_w_ptr sets in byte_n the amount of available space to write in buffer
+    // This must then be notified via the method buffer_write_adv so that the system registers
+    // this bytes will be written
+
+    size_t byte_n;
+    uint8_t * buff_ptr = buffer_write_ptr(&connection->read_buff, &byte_n);
+    ssize_t n_received = recv(connection->cli_socket, buff_ptr, byte_n, NULL); //Flags?
+
+    if(n_received <= 0) return ERROR;
+    buffer_write_adv(&connection->read_buff, n_received);
+
+    // TODO: Now we need to parse the input we receive and set the appropiate
+    // return state.
+
+}
+
+
+
+
 
 //TODO: IMPORTANT! Define functions (where needed) for arrival, read, and write in states.
 static const struct state_definition states[] = {
     {
         .state = HELLO_READ,
+        .on_arrival = conn_read_init,
+        .on_read_ready = conn_read,
     },
     {
         .state = HELLO_WRITE,
