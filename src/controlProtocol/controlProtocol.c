c2b70a86e815b91170967d615d348d2ad4b99b01
Julian Sasso <jsasso@itba.edu.ar>
2022-11-17 15:09:41 -0300

    Define control protocol structure and states

--- src/controlProtocol/controlProtocol.c
+++ src/controlProtocol/controlProtocol.c
@@ -1,1 +1,41 @@
 #include "include/controlProtocol.h"
+
+
+
+static const struct state_definition controlProtStateDef = {
+    {
+        .state = CP_HELLO,
+    },
+    {
+        .state = CP_AUTH,
+    },
+    {
+        .state = CP_EXECUTE,
+    },
+    {
+        .state = CP_OK,
+    },
+    {
+        .state = CP_ERROR,
+    },
+}
+
+
+// struct state_definition {
+//     /**
+//      * identificador del estado: típicamente viene de un enum que arranca
+//      * desde 0 y no es esparso.
+//      */
+//     unsigned state;
+
+//     /** ejecutado al arribar al estado */
+//     void     (*on_arrival)    (const unsigned state, struct selector_key *key);
+//     /** ejecutado al salir del estado */
+//     void     (*on_departure)  (const unsigned state, struct selector_key *key);
+//     /** ejecutado cuando hay datos disponibles para ser leidos */
+//     unsigned (*on_read_ready) (struct selector_key *key);
+//     /** ejecutado cuando hay datos disponibles para ser escritos */
+//     unsigned (*on_write_ready)(struct selector_key *key);
+//     /** ejecutado cuando hay una resolución de nombres lista */
+//     unsigned (*on_block_ready)(struct selector_key *key);
+// };
--- src/controlProtocol/include/controlProtocol.h
+++ src/controlProtocol/include/controlProtocol.h
@@ -1,5 +1,33 @@
 #ifndef CONTROL_PROTOCOL_H
 #define CONTROL_PROTOCOL_H
 
+#include "../../include/stm.h"
+#include "../../include/buffer.h"
+
+#define BUFFER_SIZE 1024
+
+typedef enum controlProtState {
+    CP_HELLO,
+    CP_AUTH,
+    CP_EXECUTE,
+    CP_OK,
+    CP_ERROR
+} controlProtState;
+
+typedef struct controlProtConn {
+    // TODO: Manejar datos de la conexion
+
+    buffer * readBuffer;
+    buffer * writeBuffer;
+
+    uint8_t readBufferData[BUFFER_SIZE];
+    uint8_t writeBufferData[BUFFER_SIZE];
+
+    struct state_machine connStm;
+    struct controlProtState currentState;
+
+
+} controlProtConn;
+
 
 #endif
