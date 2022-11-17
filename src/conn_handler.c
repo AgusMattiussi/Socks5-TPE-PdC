30676c6ab77eb53100f2af1df310a3d20b444c4c
desimonef <fdesimone@itba.edu.ar>
2022-11-17 17:33:18 -0300

    small changes

--- src/conn_handler.c
+++ src/conn_handler.c
@@ -68,12 +68,12 @@ void mng_connection_read(struct selector_key * key){
     printf("Entro a mng connection read\n");
     mng_conn_model * connection = (mng_conn_model *) key->data;
     enum mng_state state = stm_handler_read(&connection->stm, key);
-    if(state == ERROR){
+    if(state == MNG_ERROR){
         //For debugging purposes
         printf("Error in stm read\n");
         close_socks5_connection(connection);
     }
-    else if(state == DONE){
+    else if(state == MNG_DONE){
         //For debugging purposes
         printf("STM is DONE\n");
         close_socks5_connection(connection);
@@ -84,11 +84,11 @@ void mng_connection_write(struct selector_key * key){
     printf("Entro a mng connection write\n");
     mng_conn_model * connection = (mng_conn_model *) key->data;
     enum mng_state state = stm_handler_write(&connection->stm, key);
-    if(state == ERROR){
+    if(state == MNG_ERROR){
         fprintf(stdout, "Error in stm writing\n");
         close_socks5_connection(connection);
     }
-    else if(state == DONE){
+    else if(state == MNG_DONE){
         fprintf(stdout, "STM is DONE\n");
         close_socks5_connection(connection);
     }
@@ -98,11 +98,11 @@ void mng_connection_block(struct selector_key * key){
     printf("Entro a socks connection block\n");
     socks_conn_model * connection = (socks_conn_model *) key->data;
     enum socks_state state = stm_handler_block(&connection->stm, key);
-    if(state == ERROR){
+    if(state == MNG_ERROR){
         fprintf(stdout, "Error in stm writing\n");
         close_socks5_connection(connection);
     }
-    else if(state == DONE){
+    else if(state == MNG_DONE){
         fprintf(stdout, "STM is DONE\n");
         close_socks5_connection(connection);
     }
